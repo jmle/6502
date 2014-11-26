686371e76c4dff25a5fb57a455582d23eecafa08
qatsindio <jmleflet@gmail.com>
2014-11-26 21:15:26 +0000

    Adding lost tests

--- 6502_test.go
+++ 6502_test.go
@@ -107,3 +107,132 @@ func TestBcsWithCarrySet(t *testing.T) {
 		t.Errorf("Wrong PC")
 	}
 }
+
+func TestAdcWithCarry(t *testing.T) {
+	cpu := Cpu{}
+	mem := &Memory{}
+	cpu.mem = mem
+	cpu.ac = 255
+	mem.Write(0, 1)
+
+	cpu.adc(0)
+
+	if cpu.p.c != 1 {
+		t.Errorf("Carry flag clear")
+	}
+	if expect := 0; cpu.ac != expect {
+		t.Errorf("Invalid result: %b != %d", cpu.ac, expect)
+	}
+}
+
+func TestAnd(t *testing.T) {
+	cpu := Cpu{}
+	mem := &Memory{}
+	cpu.mem = mem
+	cpu.ac = 240
+	mem.Write(0, 255)
+
+	cpu.and(0)
+
+	if expect := 240; cpu.ac != expect {
+		t.Errorf("Invalid result: %b != %b", cpu.ac, expect)
+	}
+	if cpu.p.n != 1 || cpu.p.z != 0 {
+		t.Errorf("Invalid processor status")
+	}
+}
+
+func TestAslaWithoutCarry(t *testing.T) {
+	cpu := Cpu{}
+	mem := &Memory{}
+	cpu.mem = mem
+	cpu.ac = 16
+
+	cpu.asla()
+
+	if expect := 32; cpu.ac != expect {
+		t.Errorf("Invalid result: %b != %b", cpu.ac, expect)
+	}
+	if cpu.p.n != 0 || cpu.p.z != 0 || cpu.p.c != 0 {
+		t.Errorf("Invalid processor status")
+	}
+}
+
+func TestAslaWithCarry(t *testing.T) {
+	cpu := Cpu{}
+	mem := &Memory{}
+	cpu.mem = mem
+	cpu.ac = 128
+
+	cpu.asla()
+
+	if expect := 0; cpu.ac != 0 {
+		t.Errorf("Invalid result: %b != %b", cpu.ac, expect)
+	}
+	if cpu.p.n != 0 || cpu.p.z != 1 || cpu.p.c != 1 {
+		t.Errorf("Invalid processor status")
+	}
+}
+
+func TestAslWithoutCarry(t *testing.T) {
+	cpu := Cpu{}
+	mem := &Memory{}
+	cpu.mem = mem
+	cpu.mem.Write(0, 16)
+
+	cpu.asl(0)
+
+	actual := cpu.mem.Read(0)
+	if expect := 32; actual != expect {
+		t.Errorf("Invalid result: %b != %b", actual, expect)
+	}
+	if cpu.p.c != 0 || cpu.p.n != 0 || cpu.p.z != 0 {
+		t.Errorf("Invalid processor status")
+	}
+}
+
+func TestAslWithCarry(t *testing.T) {
+	cpu := Cpu{}
+	mem := &Memory{}
+	cpu.mem = mem
+	cpu.mem.Write(0, 128)
+
+	cpu.asl(0)
+
+	actual := cpu.mem.Read(0)
+	if expect := 0; actual != expect {
+		t.Errorf("Invalid result: %b != %b", actual, expect)
+	}
+	if cpu.p.c != 1 || cpu.p.n != 0 || cpu.p.z != 1 {
+		t.Errorf("Invalid processor status")
+	}
+}
+
+func TestBccWithCarryClear(t *testing.T) {
+	cpu := Cpu{}
+	mem := &Memory{}
+	cpu.mem = mem
+	cpu.p.c = 0
+
+	actual := cpu.bcc(16)
+
+	if expect := true; actual != expect {
+		t.Errorf("Invalid return value: %v != %v", actual, expect)
+	}
+	if expect := 16; cpu.pc != expect {
+		t.Errorf("Wrong PC")
+	}
+}
+
+func TestBccWithCarrySet(t *testing.T) {
+	cpu := Cpu{}
+	mem := &Memory{}
+	cpu.mem = mem
+	cpu.p.c = 1
+
+	actual := cpu.bcc(16)
+
+	if expect := false; actual != expect {
+		t.Errorf("Invalid return value: %v != %v", actual, expect)
+	}
+}
