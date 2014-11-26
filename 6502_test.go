a4aa2a00d22c21d1195863d5343fc4f9bca2b7ef
gbbr <ga@stripetree.co.uk>
2014-11-26 20:08:08 +0000

    Test framework setup

--- 6502_test.go
+++ 6502_test.go
@@ -1,148 +1,69 @@
 package main
 
 import (
-	"testing"
 	"log"
+	"reflect"
+	"testing"
 )
 
-type Memory struct {
-	memory	[1<<8]int
-}
-
-func (m *Memory) Read(addr int) int {
-	return m.memory[addr]
-}
-
-func (m *Memory) Write(addr, value int) {
-	m.memory[addr] = value
-}
-
-// interprets a word as bcd
-func bcd(n int) int {
-	return (n & 0xF) + (n&0xF0>>4 * 10)
-}
-
-func TestAdcHappyPath(t *testing.T) {
-	log.Println("Test adc happy path")
-
-	cpu := Cpu{}
-	mem := &Memory{}
-	cpu.mem = mem
-	cpu.ac = 2
-	mem.Write(0, 3)
-
-	cpu.adc(0)
-
-	if (cpu.ac != 5) {
-		t.Errorf("Invalid ac value: %q != %q", cpu.ac, 5)
-	}
-	if (cpu.p.v != 0 || cpu.p.n != 0 || cpu.p.z != 0 || cpu.p.c != 0) {
-		t.Errorf("Wrong processor status")
-	}
-}
-
-func TestAdcWithOverflow(t *testing.T) {
-	log.Println("Test adc with overflow")
-
-	cpu := Cpu{}
-	mem := &Memory{}
-	cpu.mem = mem
-	cpu.ac = 20
-	mem.Write(0, 120)
-
-	cpu.adc(0)
-
-	if (cpu.p.v != 1) {
-		t.Errorf("Overflow flag clear")
-	}
-}
-
-func TestAdcWithNegative(t *testing.T) {
-	log.Println("Test adc with negative")
-
-	cpu := Cpu{}
-	mem := &Memory{}
-	cpu.mem = mem
-	cpu.mem.Write(0, 200)
-
-	cpu.adc(0)
-
-	if (cpu.p.n != 1) {
-		t.Errorf("Negative flag clear")
-	}
-}
-
-func TestAdcWithZero(t *testing.T) {
-	log.Println("Test adc with zero")
-
-	cpu := Cpu{}
-	mem := &Memory{}
-	cpu.mem = mem
-	mem.Write(0, 0)
-
-	cpu.adc(0)
-
-	if (cpu.p.z != 1) {
-		t.Errorf("Zero flag clear")
-	}
-}
-
-func TestAdcWithDecimalModeWithCarry(t *testing.T) {
-	log.Println("Test adc with decimal")
-
-	cpu := Cpu{}
-	mem := &Memory{}
-	cpu.mem = mem
-	cpu.p.d = 1			// set bcd mode
-	cpu.ac = 1
-	mem.Write(0, 153)   // 99 in bcd is 153
-
-	cpu.adc(0)
-
-	if (cpu.p.c != 1) {
-		t.Errorf("Carry flag clear")
-	}
-	if (cpu.ac != 0) {
-		t.Errorf("Invalid result: %b != %d", cpu.ac, 0)
-	}
-}
-
-func TestAdcWithDecimalWithoutCarry(t *testing.T) {
-	log.Println("Test adc with decimal")
-
-	cpu := Cpu{}
-	mem := &Memory{}
-	cpu.mem = mem
-	cpu.p.d = 1			// set bcd mode
-	cpu.ac = 64			// bcd: 40
-	mem.Write(0, 8)		// bcd: 8
-
-	cpu.adc(0)
-
-	if (cpu.p.c != 0) {
-		t.Errorf("Carry flag set")
-	}
-	if (cpu.ac != 72) {	// bcd: 48
-		t.Errorf("Invalid result: %b != %d", cpu.ac, 72)
-	}
-}
-
-func TestAdcWithCarry(t *testing.T) {
-	log.Println("Test adc with decimal")
-
-	cpu := Cpu{}
-	mem := &Memory{}
-	cpu.mem = mem
-	cpu.ac = 255
-	mem.Write(0, 1)
-
-	cpu.adc(0)
-	log.Println(cpu.ac)
-
-	if (cpu.p.c != 1) {
-		t.Errorf("Carry flag clear")
-	}
-	if (cpu.ac != 1) {
-		t.Errorf("Invalid result: %b != %d", cpu.ac, 0)
+func TestAdc(t *testing.T) {
+	for _, tt := range []struct {
+		name string
+		// Set-up
+		ac, val int
+		adc     int
+		proc    ProcStat
+		// Expected
+		expAc   int
+		expProc ProcStat
+	}{
+		{name: "Happy Path",
+			ac: 2, val: 3, adc: 0,
+			expAc: 5,
+		},
+		{name: "With overflow",
+			ac: 20, val: 120, adc: 0,
+			expAc:   140,
+			expProc: ProcStat{n: 1, v: 1},
+		},
+		{name: "With negative",
+			val:     200,
+			expAc:   200,
+			expProc: ProcStat{n: 1, v: 1},
+		},
+		{name: "With zero",
+			expProc: ProcStat{z: 1},
+		},
+		{name: "With carry",
+			ac: 255, val: 1,
+			expProc: ProcStat{c: 1, v: 1},
+		},
+		{name: "Decimal mode, without Carry",
+			ac: 64, val: 8,
+			proc:    ProcStat{d: 1},
+			expProc: ProcStat{d: 1},
+			expAc:   72,
+		},
+		{name: "Decimal mode, with carry",
+			ac: 1, val: 153,
+			proc:    ProcStat{d: 1},
+			expProc: ProcStat{c: 1, d: 1},
+		},
+	} {
+		var mem Memory
+		cpu := Cpu{
+			mem: &mem,
+			ac:  tt.ac,
+			p:   tt.proc,
+		}
+		log.Println(tt.name)
+		mem.Write(0, tt.val)
+		cpu.adc(tt.adc)
+		if !reflect.DeepEqual(cpu.p, tt.expProc) {
+			t.Errorf("Expected %+v, got %+v\n", tt.expProc, cpu.p)
+		}
+		if cpu.ac != tt.expAc {
+			t.Errorf("Expected ac %d, got %d\n", tt.expAc, cpu.ac)
+		}
 	}
 }
